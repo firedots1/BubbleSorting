e414ae8bad260088abdb7f0c14780d2a5f611990
Zhi Cheng <63010166+firedots1@users.noreply.github.com>
2020-04-17 17:03:24 -0400

    Update dictionaryWord.java

--- src/dictionaryWord.java
+++ src/dictionaryWord.java
@@ -3,31 +3,31 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class dictionaryWord {
     public static void main(String args[]) {
-        String temp;
-        int n;
-        Scanner input = new Scanner(System.in);
-        System.out.println("How many Numbers of Words?: ");
+        String temp; // String for temp
+        int n; // int for n 
+        Scanner input = new Scanner(System.in); // import of scanner 
+        System.out.println("How many Numbers of Words?: ");  // Prompts user for input of # of words.
         int numberOfWords = input.nextInt();
-        String[] NameArray = new String[numberOfWords];
+        String[] NameArray = new String[numberOfWords]; // declare the array
             for (int i = 0; i < numberOfWords; i++)
             {
-                System.out.println("enter the " + (i + 1) + " Word Here:");
+                System.out.println("enter the " + (i + 1) + " Word Here:"); // Words then goes here for filling into the array.
                 NameArray[i] = input.next();
             }
-            System.out.println("Original Order: ");
-            System.out.println(Arrays.toString(NameArray));
+            System.out.println("Original Order: "); 
+            System.out.println(Arrays.toString(NameArray)); // prints out the list of names when words was input. 
             {
                 for (int i = 0; i < numberOfWords; i++) {
-                    for (int j = i + 1; j < numberOfWords; j++)
+                    for (int j = i + 1; j < numberOfWords; j++) 
                         if (NameArray[i].compareTo(NameArray[j]) > 0) {
-                            temp = NameArray[i];
+                            temp = NameArray[i]; // Sorting begins here
                             NameArray[i] = NameArray[j];
                             NameArray[j] = temp;
                             {
                                 System.out.println("Alphabetical Order: ");
-                                System.out.println(Arrays.toString(NameArray));
+                                System.out.println(Arrays.toString(NameArray)); // Prints it in Order from A - Z  
                             }
-                        }
+                        } // It is currently missing the Reversed Order // 
                 }
             }
         }
